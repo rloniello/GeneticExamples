e08d6a3c459c993f8551e1f15240e32f0cd9da48
Russell <rloniello@gmail.com>
2022-03-03 13:55:32 -0500

    Updates to truth seeker's main.

--- TruthSeekers/main.swift
+++ TruthSeekers/main.swift
@@ -6,6 +6,63 @@
 //
 
 import Foundation
+// MARK: Step 1
+import Genetics
 
-print("Hello, World!")
+// MARK: Step 1
+// Define an object that conforms to Genetic.
+// And an Enviroment that will support it.
+var naturalEnviroment: NaturalEnvironment? = nil
 
+// MARK: Step 3
+// Define a Genetic Object, see ./Models/Seeker.swift
+
+// MARK: Step 4
+// Define your Genetic Operators as GeneticOperations.
+// All Genetic Operations take an array of Genetic Objects and return nothing.
+// Usually a Genetic Algorithm contains methods to:
+// 1 - Calculate fitness
+// 2 - Selection + Reproduction
+// 3 - Mutation
+// 4 - End Condition Check
+
+let calculateFitness: GeneticOperation = { population in
+    for (index, member) in population.enumerated() {
+        var totalFitness:Double = 0.0
+        for trait in member.chromosome {
+            totalFitness += Double(trait.value as? Int ?? 0) / 16.0
+        }
+        population[index].fitness = totalFitness
+    }
+}
+
+// You can use a built-in methods for selection or define your own.
+let selectionMethod: GeneticOperation = SelectionMethods.BasicTournamentSelection()
+
+let mutationMethod: GeneticOperation = { population in
+    for (index, _) in population.enumerated() {
+        // Do something else before mutation.
+        population[index].mutate()
+    }
+}
+
+let pauseOperation: GeneticOperation = { population in
+    population.sort(by: {$0.fitness > $1.fitness})
+    guard let mostFit = population.first as? Seeker else  { return }
+    // Update the UI or perform other tasks...
+    print("Generation \(naturalEnviroment!.currentIteration)")
+    print("Most Fit: \(mostFit.printChromosome()) : \(mostFit.fitness)")
+    // like check to see if the GA has reach an optimal solution.
+    if (mostFit.fitness >= 1.0) {
+        naturalEnviroment?.stop()
+    }
+}
+
+// MARK: Step 5
+// Define a natural enviroment to run your operations on a population of your objects.
+// By default, Populations will initalize with 1000 members.
+let operations = [calculateFitness, pauseOperation, selectionMethod, mutationMethod]
+naturalEnviroment = NaturalEnvironment(for: Seeker.self, operations: operations)
+
+// And Then Run..
+naturalEnviroment?.start()
